3ae227166f30f8f3a4ac74f785c531ee581bdef2
Declaira <adrien.queneudec2601@gmail.com>
2026-01-30 13:52:02 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,15 @@ copybutton_prompt_is_regexp = True
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build", 
+    "Thumbs.db", 
+    ".DS_Store", 
+    "**/my_module.py",      # Ignore le fichier spécifique
+    "**/main_nicegui.py",   # Ignore le fichier spécifique
+    "generated/tfmc.my_module.rst", # Nettoie les restes de l'autosummary
+    "generated/tfmc.main_nicegui.rst"
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
